c986f28dd521a84ffab81435d3141208ebbc5c6e
cekk <andrea.cecchi85@gmail.com>
2009-07-17 09:25:22 +0000

    risolto bug sulla visualizzazione della portlet e dei suoi contenuti (#156)

--- cciaa/portlet/ulteriori_approfondimenti/ulterioriapprofondimenti.py
+++ cciaa/portlet/ulteriori_approfondimenti/ulterioriapprofondimenti.py
@@ -1,5 +1,7 @@
+from Acquisition import aq_inner
 from zope.interface import implements
 from Products.CMFCore.utils import getToolByName
+from zope.component import getMultiAdapter
 
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
@@ -47,33 +49,61 @@ class Renderer(base.Renderer):
     """
     @property
     def available(self):
-        if ('ulteriori-approfondimenti' in self.context.keys() or 'ulteriori-approfondimenti' in self.context.aq_inner.aq_parent.keys()) and self.getApprofondimenti():
+        if self.getApprofondimenti():
             return True
         else:
             return False
     
     def getTitle(self):
-        """"""
-        if 'ulteriori-approfondimenti' in self.context.keys():
-            return getattr(self.context,'ulteriori-approfondimenti',False).Title()
-        elif 'ulteriori-approfondimenti' in self.context.aq_inner.aq_parent.keys():
-            return getattr(self.context.aq_inner.aq_parent,'ulteriori-approfondimenti',False).Title()
+        """ritorna il titolo della cartella ulteriori approfondimenti"""
+        path= self.hasApprofondimenti()
+        if path:
+            return self.context.unrestrictedTraverse(path).Title()
         else:
             return "Ulteriori Approfondimenti"
+
             
         
     def getApprofondimenti(self):
-        if 'ulteriori-approfondimenti' in self.context.keys():
-            folder = getattr(self.context,'ulteriori-approfondimenti',False)
-            return folder.portal_catalog(path=dict(query='/'.join(folder.getPhysicalPath()), depth=1),
+        """ritorna l'elenco degli oggetti contenuti nella cartella ulteriori approfondimenti, altrimenti torna una stringa vuota"""
+        folder_path = self.hasApprofondimenti() 
+        if folder_path:
+            return self.context.portal_catalog(path={'query': folder_path,'depth':1},
                                          sort_on='getObjPositionInParent',
                                          sort_order='asc')
-        
-        if 'ulteriori-approfondimenti' in self.context.aq_inner.aq_parent.keys():
-            folder = getattr(self.context.aq_inner.aq_parent,'ulteriori-approfondimenti',False)
-            return folder.portal_catalog(path=dict(query='/'.join(folder.getPhysicalPath()), depth=1),
-                                         sort_on='getObjPositionInParent',
-                                         sort_order='asc')
+        else:
+            return ''
+            
+    def hasApprofondimenti(self):
+        """controlla se tra i figli o nel padre è presente una cartella ulteriori approfondimenti"""
+        plone_view = getMultiAdapter((aq_inner(self.context), self.request), name='plone')
+        item_path = self.context.getPhysicalPath()
+        if plone_view.isDefaultPageInFolder():
+            result= self.context.portal_catalog(path=dict(query='/'.join(item_path[:-1]), depth=1),
+                                               portal_type='Folder',
+                                               id='ulteriori-approfondimenti')
+            if result:
+                return result[0].getPath()
+            else:
+                result2= self.context.portal_catalog(path=dict(query='/'.join(item_path[:-2]), depth=1),
+                                               portal_type='Folder',
+                                               id='ulteriori-approfondimenti')
+                if result2:
+                    return result2[0].getPath()
+                else:
+                    return ''
+        else:
+            if 'ulteriori-approfondimenti' in self.context.keys():
+                return '/'.join(item_path) +'/ulteriori-approfondimenti'
+            else:
+                result= self.context.portal_catalog(path=dict(query='/'.join(item_path[:-1]), depth=1),
+                                                    portal_type='Folder',
+                                                    id='ulteriori-approfondimenti')
+                if result:
+                    return result[0].getPath()
+                else:
+                    return ''
+
         
     render = ViewPageTemplateFile('ulterioriapprofondimenti.pt')
 
