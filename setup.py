4a865ba1b5b804696fd89e5df5c7e1e9fc4a2574
cekk <andrea.cecchi85@gmail.com>
2009-09-18 10:29:35 +0000

    aggiornata verisone

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.5-SVN_UNRELEASED'
+version = '1.0.5'
 
 setup(name='cciaa.portlet.ulteriori_approfondimenti',
       version=version,
