ad881b7f677cabfc20a7b4bfba6f3702a439a72d
keul <luca@keul.it>
2011-01-19 09:59:52 +0000

    Microfix all'egg

--- cciaa/portlet/ulteriori_approfondimenti/ulterioriapprofondimenti.py
+++ cciaa/portlet/ulteriori_approfondimenti/ulterioriapprofondimenti.py
@@ -1,32 +1,23 @@
+# -*- coding: utf-8 -*-
+
 from Acquisition import aq_inner
 from zope.interface import implements
-from Products.CMFCore.utils import getToolByName
 from zope.component import getMultiAdapter
 
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
 
-from zope import schema
 from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from cciaa.portlet.ulteriori_approfondimenti import UlterioriApprofondimentiMessageFactory as _
+#from cciaa.portlet.ulteriori_approfondimenti import UlterioriApprofondimentiMessageFactory as _
 
 
 class IUlterioriApprofondimenti(IPortletDataProvider):
-    """A portlet
-
-    It inherits from IPortletDataProvider because for this portlet, the
-    data that is being rendered and the portlet assignment itself are the
-    same.
-    """
+    """The "ulteriori approfondimenti" portlet"""
 
 class Assignment(base.Assignment):
-    """Portlet assignment.
-
-    This is what is actually managed through the portlets UI and associated
-    with columns.
-    """
+    """Portlet assignment."""
     implements(IUlterioriApprofondimenti)
 
     def __init__(self):
@@ -34,19 +25,11 @@ class Assignment(base.Assignment):
 
     @property
     def title(self):
-        """This property is used to give the title of the portlet in the
-        "manage portlets" screen.
-        """
         return "Ulteriori Approfondimenti"
 
 
 class Renderer(base.Renderer):
-    """Portlet renderer.
-
-    This is registered in configure.zcml. The referenced page template is
-    rendered, and the implicit variable 'view' will refer to an instance
-    of this class. Other methods can be added and referenced in the template.
-    """
+    """Portlet renderer."""
     @property
     def available(self):
         if self.getApprofondimenti():
@@ -75,19 +58,19 @@ class Renderer(base.Renderer):
             return ''
             
     def hasApprofondimenti(self):
-        """controlla se tra i figli o nel padre è presente una cartella ulteriori approfondimenti"""
+        """controlla se tra i figli o nel padre √® presente una cartella ulteriori approfondimenti"""
         plone_view = getMultiAdapter((aq_inner(self.context), self.request), name='plone')
         item_path = self.context.getPhysicalPath()
         if plone_view.isDefaultPageInFolder() or self.context.portal_type != 'Folder':
             result= self.context.portal_catalog(path=dict(query='/'.join(item_path[:-1]), depth=1),
-                                               portal_type='Folder',
-                                               id='ulteriori-approfondimenti')
+                                                portal_type='Folder',
+                                                id='ulteriori-approfondimenti')
             if result:
                 return result[0].getPath()
             else:
                 result2= self.context.portal_catalog(path=dict(query='/'.join(item_path[:-2]), depth=1),
-                                               portal_type='Folder',
-                                               id='ulteriori-approfondimenti')
+                                                     portal_type='Folder',
+                                                     id='ulteriori-approfondimenti')
                 if result2:
                     return result2[0].getPath()
                 else:
@@ -106,18 +89,12 @@ class Renderer(base.Renderer):
                     return result[0].getPath()
                 else:
                     return ''
-
         
     render = ViewPageTemplateFile('ulterioriapprofondimenti.pt')
 
 
 class AddForm(base.NullAddForm):
-    """Portlet add form.
-
-    This is registered in configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display. The create() method actually
-    constructs the assignment that is being added.
-    """
+    """Portlet add form."""
     form_fields = form.Fields(IUlterioriApprofondimenti)
 
     def create(self):
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.5'
+version = '1.0.6'
 
 setup(name='cciaa.portlet.ulteriori_approfondimenti',
       version=version,
-      description="",
+      description="A portlet that show the C3P \"Ulteriori Approfondimenti\" section",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -16,8 +16,8 @@ setup(name='cciaa.portlet.ulteriori_approfondimenti',
         ],
       keywords='',
       author='RedTurtle Technology',
-      author_email='info@redturtle.net',
-      url='http://plone.org',
+      author_email='sviluppoplone@redturtle.net',
+      url='https://code.redturtle.it/svn/camera_di_commercio_fe/C3P/cciaa.portlet.ulteriori_approfondimenti/trunk',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['cciaa', 'cciaa.portlet'],
@@ -29,5 +29,7 @@ setup(name='cciaa.portlet.ulteriori_approfondimenti',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
